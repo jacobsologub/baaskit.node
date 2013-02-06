1ae3b43d6ed1f929b03d3c972dad0f806a004f8d
Jacob Sologub <jacob.sologub@gmail.com>
2013-02-05 22:17:42 -0800

    Renamed routes/data.js and routes/admin.js. Minor code clean up.

--- baaskit.js
+++ baaskit.js
@@ -47,29 +47,29 @@ var BaaSKitServer = (function () {
 		this.expressApp.disable ('x-powered-by');
 		this.expressApp.use (libexpress.bodyParser());
 
-		// (admin routes)
-		var baasKitAdmin = require ('./routes/admin');
-		this.expressApp.post ('/applications/:applicationName', baasKitAdmin.createApp);
-		this.expressApp.post ('/applications/:applicationId/clientkey', baasKitAdmin.generateAppClientKey);
-		this.expressApp.get ('/applications/:applicationId', baasKitAdmin.getApp);
-		this.expressApp.get ('/applications', baasKitAdmin.getAllApps);
-		this.expressApp.delete ('/applications/:applicationId', baasKitAdmin.deleteApp);
+		// (applications routes)
+		var applications = require ('./routes/applications');
+		this.expressApp.post ('/applications/:applicationName', applications.createApp);
+		this.expressApp.post ('/applications/:applicationId/clientkey', applications.generateAppClientKey);
+		this.expressApp.get ('/applications/:applicationId', applications.getApp);
+		this.expressApp.get ('/applications', applications.getAllApps);
+		this.expressApp.delete ('/applications/:applicationId', applications.deleteApp);
 
-		// (data routes)
-		var baasKitData = require ('./routes/data');
-		this.expressApp.post ('/collections/:collectionName', baasKitData.post);
-		this.expressApp.get ('/collections/:collectionName/:objectId', baasKitData.get);
-		this.expressApp.get ('/collections/:collectionName', baasKitData.get);
-		this.expressApp.put ('/collections/:collectionName/:objectId', baasKitData.put);
-		this.expressApp.put ('/collections/:collectionName', baasKitData.put);
-		this.expressApp.delete ('/collections/:collectionName/:objectId', baasKitData.delete);
-		this.expressApp.delete ('/collections/:collectionName', baasKitData.delete);
+		// (collections routes)
+		var collections = require ('./routes/collections');
+		this.expressApp.post ('/collections/:collectionName', collections.post);
+		this.expressApp.get ('/collections/:collectionName/:objectId', collections.get);
+		this.expressApp.get ('/collections/:collectionName', collections.get);
+		this.expressApp.put ('/collections/:collectionName/:objectId', collections.put);
+		this.expressApp.put ('/collections/:collectionName', collections.put);
+		this.expressApp.delete ('/collections/:collectionName/:objectId', collections.delete);
+		this.expressApp.delete ('/collections/:collectionName', collections.delete);
     }
 
     BaaSKitServer.prototype.start = function (port) {
 
         this.expressApp.listen (port);
-    }
+    };
 
     return BaaSKitServer;
 })();
@@ -83,8 +83,8 @@ var BaaSKit = (function () {
 
 	//==============================================================================
 	/** Creates a BaaSKit object. */
-    function BaaSKit() {
-
+    function BaaSKit() 
+    {
     	var packageInfo = require ('./package');
     	var version = packageInfo ['version'];
 
@@ -100,11 +100,11 @@ var BaaSKit = (function () {
 		if (libcommander.createapp || libcommander.deleteapp || libcommander.listapps || libcommander.generateclientkey)
 		{
 			var admin = require ('./baaskitadmin');
-			var baasKitAdmin = new admin.BaaSKitAdmin (true);
+			var applications = new admin.BaaSKitAdmin (true);
 
 			if (libcommander.createapp && libcommander.createapp.length)
 			{
-				baasKitAdmin.createApplication (libcommander.createapp, function (error, newApplication) {
+				applications.createApplication (libcommander.createapp, function (error, newApplication) {
 
 					console.log (!error ? newApplication : error ['message']);
 					process.exit (error);
@@ -112,7 +112,7 @@ var BaaSKit = (function () {
 			}
 			else if (libcommander.deleteapp && libcommander.deleteapp.length)
 			{
-				baasKitAdmin.deleteApplication (libcommander.deleteapp, function (error) {
+				applications.deleteApplication (libcommander.deleteapp, function (error) {
 
 					console.log (!error ? "Application deleted successfully." : error ['message']);
 					process.exit (error);
@@ -120,7 +120,7 @@ var BaaSKit = (function () {
 			}
 			else if (libcommander.listapps)
 			{
-				baasKitAdmin.listAllApplications (function (error, applicationList) {
+				applications.listAllApplications (function (error, applicationList) {
 
 					console.log (!error ? applicationList : error ['message']);
 					process.exit (error);
@@ -128,7 +128,7 @@ var BaaSKit = (function () {
 			}
 			else if (libcommander.generateclientkey && libcommander.generateclientkey.length)
 			{
-				baasKitAdmin.generateApplicationClientKey (libcommander.generateclientkey, function (error) {
+				applications.generateApplicationClientKey (libcommander.generateclientkey, function (error) {
 
 					console.log (!error ? "Application client key generated successfully." : error ['message']);
 					process.exit (error);
