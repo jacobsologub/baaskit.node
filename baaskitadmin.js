jacobsologub/baaskit.node

commit 5118fd3e332d3785892e1f05fdfd4cc92540a5b1

Fixed variable declaration mistake in BaaSKitServer, BaaSKitAdmin and AppDbPool classes. Theses were intended to be "non-static".

baaskitadmin.js
@@ -49,7 +49,7 @@ var BaaSKitAdmin = (function () {
 	/** Creates a BaaSKitAdmin object. */
     function BaaSKitAdmin (asksForUserConfirmation_) 
     {
-    	asksForUserConfirmation = asksForUserConfirmation_;
+    	this.asksForUserConfirmation = asksForUserConfirmation_;
     }
 
     //==========================================================================
@@ -240,8 +240,6 @@ var BaaSKitAdmin = (function () {
 			}
 		});
     };
-
-    asksForUserConfirmation = false;
 
     return BaaSKitAdmin;
 })();

baaskit.js
@@ -40,37 +40,37 @@ var BaaSKitServer = (function () {
 	/** Creates a BaaSKitServer object. */
     function BaaSKitServer()
     {
-    	expressApp = libexpress();
+    	this.expressApp = libexpress();
 
-		expressApp.configure (function() {
-			expressApp.set ('title', 'baaskit');
-			expressApp.disable ('x-powered-by');
-			expressApp.use (libexpress.bodyParser());
-		});
+    	// (set settings for all environments)
+		this.expressApp.set ('title', 'baaskit');
+		this.expressApp.disable ('x-powered-by');
+		this.expressApp.use (libexpress.bodyParser());
 
+		// (admin routes)
 		var baasKitAdmin = require ('./routes/admin');
-		expressApp.post ('/applications/:applicationName', baasKitAdmin.createApp);
-		expressApp.post ('/applications/:applicationId/clientkey', baasKitAdmin.generateAppClientKey);
-		expressApp.get ('/applications/:applicationId', baasKitAdmin.getApp);
-		expressApp.get ('/applications', baasKitAdmin.getAllApps);
-		expressApp.delete ('/applications/:applicationId', baasKitAdmin.deleteApp);
+		this.expressApp.post ('/applications/:applicationName', baasKitAdmin.createApp);
+		this.expressApp.post ('/applications/:applicationId/clientkey', baasKitAdmin.generateAppClientKey);
+		this.expressApp.get ('/applications/:applicationId', baasKitAdmin.getApp);
+		this.expressApp.get ('/applications', baasKitAdmin.getAllApps);
+		this.expressApp.delete ('/applications/:applicationId', baasKitAdmin.deleteApp);
 
+		// (data routes)
 		var baasKitData = require ('./routes/data');
-		expressApp.post ('/collections/:collectionName', baasKitData.post);
-		expressApp.get ('/collections/:collectionName/:objectId', baasKitData.get);
-		expressApp.get ('/collections/:collectionName', baasKitData.get);
-		expressApp.put ('/collections/:collectionName/:objectId', baasKitData.put);
-		expressApp.put ('/collections/:collectionName', baasKitData.put);
-		expressApp.delete ('/collections/:collectionName/:objectId', baasKitData.delete);
-		expressApp.delete ('/collections/:collectionName', baasKitData.delete);
+		this.expressApp.post ('/collections/:collectionName', baasKitData.post);
+		this.expressApp.get ('/collections/:collectionName/:objectId', baasKitData.get);
+		this.expressApp.get ('/collections/:collectionName', baasKitData.get);
+		this.expressApp.put ('/collections/:collectionName/:objectId', baasKitData.put);
+		this.expressApp.put ('/collections/:collectionName', baasKitData.put);
+		this.expressApp.delete ('/collections/:collectionName/:objectId', baasKitData.delete);
+		this.expressApp.delete ('/collections/:collectionName', baasKitData.delete);
     }
 
     BaaSKitServer.prototype.start = function (port) {
 
-        expressApp.listen (port);
+        this.expressApp.listen (port);
     }
 
-    var expressApp = null;
     return BaaSKitServer;
 })();
 

routes/data.js
@@ -52,7 +52,9 @@ var AppDbPool = (function () {
 	*/
     function AppDbPool (maxNumberOfApps)
     {
-    	maxNumApps = maxNumberOfApps;
+    	this.maxNumApps = Math.max (maxNumberOfApps, 1);
+    	this.lookup = {};
+    	this.stack = [];
     }
 
     /** Returns a shared database object using the current socket connection. 
@@ -61,32 +63,28 @@ var AppDbPool = (function () {
     */
     AppDbPool.prototype.getDb = function (appId) {
 
-    	if (stack.length >= maxNumApps)
+    	if (this.stack.length >= this.maxNumApps)
 		{
-			var poppedAppId = stack.pop();
-			delete lookup [poppedAppId];
+			var poppedAppId = this.stack.pop();
+			delete this.lookup [poppedAppId];
 		}
 
 		var result = null;
 
-		if (lookup[appId] != null)
+		if (this.lookup[appId] != null)
 		{
-			result = lookup [appId];
+			result = this.lookup [appId];
 		}
 		else
 		{
-			stack.unshift (appId);
+			this.stack.unshift (appId);
 
 			result = kDatabaseConnection.db (appId);
-			lookup[appId] = result;
+			this.lookup[appId] = result;
 		}
 
 		return result;
 	};
-
-	var maxNumApps = 64;
-    var lookup = {};
-	var stack = [];
 
     return AppDbPool;
 

commit 8bbd208f4b7b98b96ff30c6c37c66a5e99701535

Fixed issue in BaaSKitAdmin class that was introduced in commit 5118fd3e.

baaskitadmin.js
@@ -88,6 +88,7 @@ var BaaSKitAdmin = (function () {
     BaaSKitAdmin.prototype.deleteApplication = function (applicationId, callback) {
 
     	var colleciton = BaaSKitDb.getInstance().getMainDb().collection ('applications');
+    	var asksForUserConfirmation = this.asksForUserConfirmation;
 
 		colleciton.findOne ({ 'id' : applicationId }, function (error, item) {
 
@@ -115,7 +116,7 @@ var BaaSKitAdmin = (function () {
 					}
 				};
 
-				if (this.asksForUserConfirmation)
+				if (asksForUserConfirmation)
 				{
 					libcommander.confirm ('Are you sure you want to delete this application? ', function (ok) {
 
@@ -183,6 +184,7 @@ var BaaSKitAdmin = (function () {
     BaaSKitAdmin.prototype.generateApplicationClientKey = function (applicationId, callback) {
 
 		var colleciton = BaaSKitDb.getInstance().getMainDb().collection ('applications');
+		var asksForUserConfirmation = this.asksForUserConfirmation;
 		
 		colleciton.findOne ({ 'id' : applicationId }, function (error, item) {
 
@@ -213,7 +215,7 @@ var BaaSKitAdmin = (function () {
 					}
 				};
 
-				if (this.asksForUserConfirmation)
+				if (asksForUserConfirmation)
 				{
 					libcommander.confirm ('Are you sure you want to generate a new client key for this application? ', function (ok) {
 